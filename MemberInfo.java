5dd33a20ecf61453a73749d9903a56e0c048d295
Lance Phoenix <108348802+lancepr1@users.noreply.github.com>
2025-05-04 20:05:17 -0400

    Create MemberInfo.java

---
+++ MemberInfo.java
@@ -0,0 +1,29 @@
+public class MemberInfo
+{
+private String name;
+private int gradYear;
+private Boolean hasGoodStanding;
+public MemberInfo(String name, int gradYear, boolean hasGoodStanding)
+{
+    this.name = name;
+    this.gradYear = gradYear;
+    this.hasGoodStanding = hasGoodStanding;
+}
+public String toString()
+{
+    String s = "\nName: " + name + "\n";
+    s+= "Year: " + gradYear + "\n";
+    return s + "Good Standing: " + hasGoodStanding + "\n";
+}
+/** Returns the graduation year of the club member. */
+public int getGradYear()
+{
+    return gradYear;
+}
+/** Returns true if the member is in good standing and false otherwise. */
+public boolean inGoodStanding()
+{
+    return hasGoodStanding;
+}
+// There may be instance variables, constructors, and methods that are not shown.
+}
